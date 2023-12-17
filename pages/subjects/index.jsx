985579f4485b93bd343967954dfc67b190307f68
suparansharma <suparansharmap@gmail.com>
2023-12-18 00:14:44 +0600

    subject list done

--- pages/subjects/index.jsx
+++ pages/subjects/index.jsx
@@ -2,12 +2,26 @@
 import { SUBJECT_END_POINT } from '@/constants';
 import { QUERY_KEYS } from '@/constants/queryKeys';
 import { useGetAllData } from '@/utils/hooks/useGetAllData';
-import React, { useEffect, useState } from 'react'
+import { DeleteOutlined, EditOutlined, EyeOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { Row, Table, Tag } from 'antd';
+import Link from "next/link";
 const Subjects = () => {
+
+    /*** Storing data start */
     const [search, setSearch] = useState('');
     const [page, setPage] = useState(1);
     const [limit, setLimit] = useState(10);
+    const [perPage, setPerPage] = useState(10);
+    /*** Storing data end */
+
+
+
+
+    /*** Fetch All Subject List Start  */
 
     const {
         data: subjectList,
@@ -15,20 +29,118 @@ const Subjects = () => {
         refetch: fetchSubjectList,
     } = useGetAllData(QUERY_KEYS.GET_ALL_SUBJECT_LIST, SUBJECT_END_POINT.get(page, limit, search, ""));
 
-    console.log("subjectList", subjectList);
+    /*** Fetch All Subject List end  */
+
+
+
+    /*** Pagination Start  */
+    const pagination = {
+        total: subjectList?.total,
+        current: page,
+        pageSize: perPage,
+        defaultPageSize: 10,
+        showSizeChanger: true,
+        pageSizeOptions: ['2', '5', '10', '20', '30']
+    };
+
+    const onChange = (
+        pagination,
+        filters,
+        sorter,
+        extra
+    ) => {
+        setPage(pagination.current);
+        setLimit(pagination.pageSize);
+        console.log(pagination, filters, sorter, extra);
+    };
+
+    /*** Pagination End  */
+
+
+
+
+    const columns = [
+        {
+            title: 'SL',
+            fixed: 'left',
+            render: (text, record, index) => index + 1
+        },
+        {
+            title: 'Subject Code',
+            dataIndex: 'subjectId',
+            // fixed: 'left',
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Status',
+            dataIndex: 'status',
+            render: (status) => (
+                status ? <Tag color='green'>ACTIVE</Tag> : <Tag color='volcano'>INACTIVE</Tag>
+            ),
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            fixed: 'right',
+            width: 100,
+            render: (row) => actionButton(row), // You need to define actionButton function
+        },
+    ];
+
+    const actionButton = (row) => {
+        return (
+            <>
+                <Row justify="space-between" style={{ display: 'flex', alignItems: 'center' }}>
+                    <a onClick={() => handleViewOpen(row)} style={{ color: 'green' }}>
+                        <EyeOutlined style={{ fontSize: '22px' }} />
+                    </a>
+
+                    <a onClick={() => handleOpen(row)} className="text-primary" >
+                        <EditOutlined style={{ fontSize: '22px' }} />
+                    </a>
+
+                    <a onClick={() => showDeleteConfirm(row._id, row.name)} className="text-danger" >
+                        <DeleteOutlined style={{ fontSize: '22px' }} />
+                    </a>
+                </Row>
+            </>
+        );
+    };
 
 
     return (
         <div className="flex flex-col gap-10">
-
             <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-                <div className="py-6 px-4 md:px-6 xl:px-7.5">
+                <div className="py-6 px-4 md:px-6 xl:px-7.5 flex justify-between items-center border-b border-stroke dark:border-strokedark">
                     <h4 className="text-xl font-semibold text-black dark:text-white">
                         All Subjects
                     </h4>
+                    <button
+                        href="#"
+                        className="inline-flex items-center justify-center rounded-full bg-primary py-2 px-5 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
+                    >
+                        Add
+                        <span className="button-icon-space ml-5">
+                            <FontAwesomeIcon icon={faPlusCircle} />
+                        </span>
+                    </button>
                 </div>
+
+                <Table
+                    className="border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark text-black dark:text-white"
+                    columns={columns}
+                    dataSource={subjectList?.data}
+                    scroll={{ x: 'max-content' }}
+                    pagination={pagination}
+                    onChange={onChange}
+                />
             </div>
         </div>
+
+
     )
 }
 
