d33e5005a539b340923dffa864d8669805363941
suparansharma <suparansharmap@gmail.com>
2023-12-20 17:16:59 +0600

    update

--- pages/subjects/SubjectForm.jsx
+++ pages/subjects/SubjectForm.jsx
@@ -1,28 +1,99 @@
-import React, { useState } from 'react'
+import ToastMessage from '@/components/Toast';
+import { SUBJECT_END_POINT } from '@/constants';
+import { post, put } from '@/helpers/api_helper';
+import React, { useCallback, useEffect, useState } from 'react'
 
-const SubjectForm = ({ isOpen, onClose,setEditData }) => {
 
-  const [suject,setSubjet] = useState({})
 
-console.log("suject is",suject);
+
+
+
+
+const SubjectForm = ({ isOpen, onClose, setEditData,isParentRender }) => {
+
+  const [subject, setSubject] = useState({ name: '', status: '' });
+  const [loading, setLoading] = useState(false);
+
+  const notify = useCallback((type, message) => {
+    ToastMessage({ type, message });
+  }, []);
+
+
+  useEffect(() => {
+    if (setEditData === null) {
+      setSubject({ name: '', status: '' });
+    } else {
+      setSubject({
+        name: setEditData.name || '',
+        status: setEditData.status || '',
+      });
+    }
+  }, [setEditData?._id, setEditData]);
+
+
+
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-
-    setSubjet((prev) => ({
+    setSubject((prev) => ({
       ...prev,
       [name]: value,
     }));
   };
 
 
-  
-  const handleSubmit = async (e) => {
 
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  
-  
-  }
+    // Add your form submission logic here
+    setLoading(true);
+    if (setEditData?._id) {
+      try {
+        const update = await put(SUBJECT_END_POINT.update(setEditData._id), subject);
+        if (update.status == 'SUCCESS') {
+          notify('success', update.message);
+          if (isParentRender) {
+            isParentRender(true);
+          }
+          onClose();
+        }
+      } catch (error) {
+        notify('error', update.errorMessage);
+        setLoading(false);
+      }
+    } else {
+      try {
+        const response = await post(SUBJECT_END_POINT.create(), subject);
+        if (response.status === 'SUCCESS') {
+          notify('success', response.message);
+          if (isParentRender) {
+            isParentRender(true);
+          }
+          onClose();
+        } else {
+          notify('error', response.errorMessage);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log(error)
+        notify('error', error.message);
+        setLoading(false);
+      }
+
+      // setLoading(false);
+
+    }
+
+    onClose();
+    setLoading(false);
+  };
+
+
+
+
+
+
+
 
 
   return (
@@ -36,10 +107,13 @@ console.log("suject is",suject);
               {/* Modal content */}
               <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                 <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                  Create New Subject
+                   {setEditData?._id?"Update Subject":"Create New Subject"} 
                 </h3>
                 <button
-                  onClick={onClose}
+                    onClick={() => {
+                      onClose();
+                      setSubject({});
+                    }}
                   type="button"
                   className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                   data-modal-toggle="crud-modal"
@@ -79,6 +153,7 @@ console.log("suject is",suject);
                       className="bg-gray border-stroke border-gray-300 text-black text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
                       placeholder="Type product name"
                       required=""
+                      defaultValue={subject?.name}
                       onChange={handleChange}
                     />
                   </div>
@@ -91,9 +166,11 @@ console.log("suject is",suject);
                       Status
                     </label>
                     <select
+                      name='status'
                       id="status"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500  dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
                       onChange={handleChange}
+                      value={subject?.status}
                     >
                       <option selected="">Select category</option>
                       <option value={true}>Active</option>
@@ -120,7 +197,9 @@ console.log("suject is",suject);
                         clipRule="evenodd"
                       />
                     </svg>
-                    Add new Subject
+                    {setEditData?._id?"Update Subject":"Create New Subject"} 
+
+                    {/* Add new Subject */}
                   </button>
                 </div>
 
